c89b7a0299151ef468c5f308cb8d8111eac35297
AarushModak <aarushmodak2302@gmail.com>
2026-01-29 04:26:35 +0530

    initial commit

---
+++ Task2.py
@@ -0,0 +1,8 @@
+# Task 2: Using the Math Module for Calculations
+import math
+
+num = int(input("Enter a number: "))
+
+print(f"Square root: {math.sqrt(num)}")
+print(f"Logarithm: {math.log(num)}")
+print(f"Sine: {math.sin(num)}")
